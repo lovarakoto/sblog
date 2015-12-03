36aa95e45b50ad330e5abced268dffbe45631e8b
lovarakoto <lovarakoto1@gmail.com>
2015-12-03 19:01:13 +0100

    creation du formulaire de contact et utilisation de Swift Mailer

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -5,6 +5,7 @@ namespace Blogger\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Blogger\BlogBundle\Entity\Enquiry;
 use Blogger\BlogBundle\Form\EnquiryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 class PageController extends Controller
@@ -30,12 +31,33 @@ class PageController extends Controller
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
     
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-
-
-        // //  // partie 2
-        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+    
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+    
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from sblog')
+                    ->setFrom('symblog55@gmail.com')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+        
+                $request->getSession()->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+        
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+    
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
     
     // public function sidebarAction()
